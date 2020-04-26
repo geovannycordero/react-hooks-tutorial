8df9579c1100ce92ec277d0e111625907bc2c544
Geovanny Cordero <geovanny@pm.me>
2020-04-26 17:56:54 -0600

    Use state overview

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const InputElement = () => {
+
+  const [inputText, setInputText] = useState("");
+  const [histiryList, setHistoryList] = useState([]);
+
+  return <div>
+    <input 
+      onChange={(e) => {
+        setInputText(e.target.value)
+        setHistoryList([...histiryList, e.target.value])
+      }}
+      placeholder="Enter some text" 
+    />
+    <br/>
+    {inputText}
+    <ul>
+      {histiryList.map((rec) => {
+        return <div>{rec}</div>
+      })}
+    </ul>
+  </div>
+}
+
+export default InputElement;
