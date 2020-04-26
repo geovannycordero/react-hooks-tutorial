05d23b0a6ebddeebb1519826a0f9adf4ed5c1549
Geovanny Cordero <geovanny@pm.me>
2020-04-26 17:53:02 -0600

    First commit

---
+++ .next/static/webpack/static/development/pages/index.js.b7168044665931c7fe2d.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/home/alberto/code/hello-next/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var InputElement = function InputElement() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(""),
+      inputText = _useState[0],
+      setInputText = _useState[1];
+
+  return __jsx("input", {
+    onChange: function onChange(e) {},
+    placeholder: "Enter some text",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 10
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (InputElement);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b7168044665931c7fe2d.hot-update.js.map
